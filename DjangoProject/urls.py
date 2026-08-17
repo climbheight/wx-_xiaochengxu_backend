e0b541c2901c65cac8395cc1ff51222694459487
climbheight <u202413681@hust.edu.cn>
2026-08-17 23:38:15 +0800

    配置了admin 图片

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("media/<path:path>",serve,{"document_root":settings.MEDIA_ROOT}),
 ]
